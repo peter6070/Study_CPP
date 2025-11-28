7de50434d0dc62d7e8e4f8733d1294d2755c567a
peter6070 <peter6070@naver.com>
2025-11-28 23:17:56 +0900

    Setting C++ environment on Labtop
    노트북에 C++ 개발 환경(VScode)

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,8 @@
-//#include <iostream>
-//
-//int main(void) {
+#include <iostream>
 
+int main(void) {
+    for(int i=0;i<5;i++)
+        std::cout<<"Hello World"<<i<<std::endl;
 
-//	return 0; //클래스 종료 선언
-//}
+    return 0;
+}
