b94ca6bfbed77a4014bd86f1d238cdc6307d10cd
peter6070 <peter6070@naver.com>
2025-12-20 22:23:56 +0900

    [Cpt 4-2] Complete Q 4-2
    챕터 4-2 문제 4-2 완료

--- Cpt4_2_Encapsulation.cpp
+++ Cpt4_2_Encapsulation.cpp
@@ -1,49 +1,93 @@
 #include <iostream>
 using namespace std;
 
+//클래스 캡슐화
+//class SinivelCap {
+//public:
+//	void Take() const {
+//		cout << "SinivelCap" << endl;
+//	}
+//};
+//
+//class SneezeCap {
+//public:
+//	void Take() const {
+//		cout << "SneezeCap" << endl;
+//	}
+//};
+//
+//class SnuffleCap {
+//public:
+//	void Take() const {
+//		cout << "SnuffleCap" << endl;
+//	}
+//};
+//
+//class CONTAC600 {
+//	SinivelCap sin;
+//	SneezeCap sne;
+//	SnuffleCap snu;
+//
+//public:
+//	void Take() const {
+//		sin.Take();
+//		sne.Take();
+//		snu.Take();
+//	}
+//};
+//
+//class ColdPatient {
+//public:
+//	//캡슐화의 무너진 예시
+//	/*void TakeSinivelCap(const SinivelCap& cap) const { cap.Take(); }
+//	void TakeSneezeCap(const SneezeCap& cap) const { cap.Take(); }
+//	void TakeSnuffleCap(const SnuffleCap& cap) const { cap.Take(); }*/
+//
+//	void TakeCONTAC600(const CONTAC600& cap) const { cap.Take(); }
+//};
 
-class SinivelCap {
+//------------------------------------------------
+//문제 4-2
+class Point {
+	int xpos, ypos;
 public:
-	void Take() const {
-		cout << "SinivelCap" << endl;
+	void Init(int x, int y) {
+		xpos = x;
+		ypos = y;
+	}
+	void ShowPointInfo() const {
+		cout << "[" << xpos << "," << ypos << "]" << endl;
 	}
 };
 
-class SneezeCap {
+class Circle {
+	int radius;
+	Point center;
 public:
-	void Take() const {
-		cout << "SneezeCap" << endl;
+	void Init(int x, int y, int r) {
+		radius = r;
+		center.Init(x,y);
+	}
+	void ShowCircleInfo() const {
+		cout << "radius: " << radius << endl;
+		center.ShowPointInfo();
 	}
 };
 
-class SnuffleCap {
+class Ring {
+	Circle innerCircle;
+	Circle outterCircle;
 public:
-	void Take() const {
-		cout << "SnuffleCap" << endl;
+	void Init(int inX, int inY, int inR, int outX, int outY, int outR) {
+		innerCircle.Init(inX, inY, inR);
+		outterCircle.Init(outX, outY, outR);
 	}
-};
-
-class CONTAC600 {
-	SinivelCap sin;
-	SneezeCap sne;
-	SnuffleCap snu;
-
-public:
-	void Take() const {
-		sin.Take();
-		sne.Take();
-		snu.Take();
+	void ShowRingInfo() const {
+		cout << "Inner Circle Info.." << endl;
+		innerCircle.ShowCircleInfo();
+		cout << "Outter Circle Info.." << endl;
+		outterCircle.ShowCircleInfo();
 	}
-};
-
-class ColdPatient {
-public:
-	//캡슐화의 무너진 예시
-	/*void TakeSinivelCap(const SinivelCap& cap) const { cap.Take(); }
-	void TakeSneezeCap(const SneezeCap& cap) const { cap.Take(); }
-	void TakeSnuffleCap(const SnuffleCap& cap) const { cap.Take(); }*/
-
-	void TakeCONTAC600(const CONTAC600& cap) const { cap.Take(); }
 };
 
 int main(void) {
@@ -56,9 +100,15 @@ int main(void) {
 	sufferer.TakeSneezeCap(zcap);
 	sufferer.TakeSnuffleCap(ncap);*/
 
-	CONTAC600 cap;
+	/*CONTAC600 cap;
 	ColdPatient sufferer;
-	sufferer.TakeCONTAC600(cap);
+	sufferer.TakeCONTAC600(cap);*/
+
+	//------------------------------------------------------------
+	//문제 4-2. Circle 클래스
+	Ring ring;
+	ring.Init(1, 1, 4, 2, 2, 9);
+	ring.ShowRingInfo();
 
 	return 0;
 }
