9eaf92418436c69ba2108007f143e347a0d54d12
peter6070 <peter6070@naver.com>
2025-12-22 23:27:03 +0900

    [Cpt 4-3] Study Default Constructor
    챕터 4-3 디폴트 생성자 공부

--- Cpt4_3_Constructor_Destructor.cpp
+++ Cpt4_3_Constructor_Destructor.cpp
@@ -27,36 +27,67 @@ using namespace std;
 //	}
 //};
 
-//멤버변수 이니셜라이저 초기화
-class SoSimple {
-	int num1;
-public:
-	SoSimple(int n1) : num1(n1) //변수 num1을 매개변수 n1으로 초기화
-	{
+////멤버변수 이니셜라이저 초기화
+//class SoSimple {
+//	int num1;
+//public:
+//	SoSimple(int n1) : num1(n1) //변수 num1을 매개변수 n1으로 초기화
+//	{
+//
+//	}
+//};
 
+//--------------------------------------------------------
+
+class AAA {
+public:
+	AAA() {
+		cout << "empty object" << endl;
+	}
+	void ShowYourName() {
+		cout << "I'm class AAA" << endl;
 	}
 };
 
+class BBB {
+	AAA& ref; //참조자가 멤버변수로 선언. 이니셜라이저를 통해 초기화
+	const int& num; //const 참조자 선언. 이니셜라이저를 통해 정수형 상수로도 초기화 가능
 
-//int main(void) {
-//	/*SimpleClass sc1;
-//	sc1.ShowData();
-//
-//	SimpleClass sc2(100);
-//	sc2.ShowData();
-//
-//	SimpleClass sc3(100, 200);
-//	sc3.ShowData();*/
-//
-//	//SimpleClass sc1(); //지역적으로 함수 원형 선언
-//	//SimpleClass mysc = sc1(); //sc1 함수를 호출하여, 이 때 반환되는 객체 값으로 mysc 객체 초기화
-//	//mysc.ShowData();
-//
-//	//-------------------------------------------------
-//
-//
-//	return 0;
-//}
+public:
+	BBB(AAA& r, const int& n)
+		: ref(r), num(n) {
+
+	}
+	void ShowYourName() {
+		ref.ShowYourName();
+		cout << "and" << endl;
+		cout << "I ref num" << num << endl;
+	}
+};
+
+int main(void) {
+	/*SimpleClass sc1;
+	sc1.ShowData();
+
+	SimpleClass sc2(100);
+	sc2.ShowData();
+
+	SimpleClass sc3(100, 200);
+	sc3.ShowData();*/
+
+	//SimpleClass sc1(); //지역적으로 함수 원형 선언
+	//SimpleClass mysc = sc1(); //sc1 함수를 호출하여, 이 때 반환되는 객체 값으로 mysc 객체 초기화
+	//mysc.ShowData();
+
+	//-------------------------------------------------
+
+	AAA obj1;
+	BBB obj2(obj1, 20);
+	obj2.ShowYourName();
+
+	return 0;
+}
+
 
 ////함수 정의
 //SimpleClass sc1() {
