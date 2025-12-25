d7e24ba6ce7cbdf6beaa5f0d37d690ac98167927
peter6070 <peter6070@naver.com>
2025-12-25 23:57:49 +0900

    [Cpt 4-4] Add Comments
    챕터 4-4 주석 추가

--- Cpt4_4_Class_Array_thisPointer.cpp
+++ Cpt4_4_Class_Array_thisPointer.cpp
@@ -6,25 +6,30 @@ class Person {
 	char* name;
 	int age;
 public:
+	//인자받는 생성자
 	Person(char* myname, int myage) {
 		int len = strlen(myname) + 1;
 		name = new char[len];
 		strcpy_s(name, len, myname);
 		age = myage;
 	}
+	//생성자
 	Person() {
 		name = NULL;
 		age = 0;
 		cout << "called Person()" << endl;
 	}
+	//이름, 나이 설정
 	void SetPersonInfo(char* myname, int myage) {
 		name = myname;
 		age = myage;
 	}
+	//정보 출력
 	void ShowPersonInfo() const {
 		cout << "Name: " << name << ", ";
 		cout << "Age: " << age << endl;
 	}
+	//소멸자
 	~Person() {
 		delete[] name;
 		cout << "called Destructor" << endl;
@@ -38,6 +43,7 @@ int main(void) {
 	int age;
 	int len;
 
+	//for문을 이용해 배열의 각 객체마다 이름과 나이 정보를 입력받아 객체 초기화
 	for (int i = 0; i < 3; i++) {
 		cout << "Name: ";
 		cin >> nameStr;
