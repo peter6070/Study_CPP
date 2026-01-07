bd9e64b4b0162e37152e10751cb943614d68cbe1
peter6070 <peter6070@naver.com>
2026-01-07 23:59:21 +0900

    [Cpt 4-5] Account Program ver2 WIP
    챕터 4-5 계좌 관리 프로그램 프로젝트 진행중

--- Cpt4_5_OOP_Project2_UpgradingAccountProgram.cpp
+++ Cpt4_5_OOP_Project2_UpgradingAccountProgram.cpp
@@ -4,30 +4,13 @@ using namespace std;
 
 //계좌 관리 프로그램 0.2
 //구조체 -> 클래스
+//고객 이름 저장 시 동적할당 형태로 저장(Acount 클래스는 멤버변수로 문자열 포인터를 지니고 있어야함)
+//객체 배열 선언하지 말고, 객체 포인터 배열 선언해 객체 저장(구조체 배열 -> 포인터 배열)
 
-//기능1. 계좌개설
-//기능2. 입금
-//기능3. 출금
-//기능4. 전체고객 잔액조회
-
-//가정
-// 통장 계좌번호 중복없음(중복검사 안함)
-// 입금, 출금액은 무조건 0보다 큼(오류검사 안함)
-// 고객 계좌정보는 계좌번호, 이름, 잔액 세가지만 저장
-// 둘 이상의 고객 정보 저장을 위해 배열 사용
-// 계좌번호는 정수형
 
 //최대 고객 수
 const int MAX_CLIENT = 100;
 
-struct Client {
-	int id;
-	string name;
-	int balance;
-};
-
-//전역변수
-Client accArr[MAX_CLIENT];
 int accNum = 0; //저장된 고객수
 
 //함수 선언
@@ -39,13 +22,29 @@ void Withdraw();
 void ShowAllAccInfo();
 
 
-int main(void) {
-	//int index = 0; //인덱스
-	int selectNum = 0; //번호 선택
+//Account 클래스
+class Account {
+	int id;
+	char* strptr;
+	int balance;
+public:
+	Account(int myId, char *nameStr, int myBalance) 
+		:id(myId), balance(myBalance)
+	{
+		int len = strlen(nameStr)+1;
+		strptr = new char[len];
+		strcpy_s(strptr, len, nameStr);
+	}
+	~Account() {
+		delete strptr;
+	}
+};
 
-	//bool isExit = true; //프로그램 종료 상태
-	//int searchID = 0; //입금 계좌 ID
-	//int changeAmount = 0; //추가 입금액
+int main(void) {
+
+	Account* accArr[MAX_CLIENT];
+
+	int selectNum = 0; //번호 선택
 
 
 	while (true) {
@@ -92,13 +91,17 @@ void PrintMenu() {
 
 //계좌 개설
 void MakeAccount() {
+	int id;
+	char name[100];
+	int balance;
 	cout << "[Account opening]" << endl;
 	cout << "Account ID: ";
-	cin >> accArr[accNum].id;
+	cin >> id;
 	cout << "Name: ";
-	cin >> accArr[accNum].name;
+	cin >> name;
 	cout << "Deposit amount: ";
-	cin >> accArr[accNum].balance;
+	cin >> balance;
+	accArr[accNum] = new Account(id, name);
 	accNum++;
 }
 
