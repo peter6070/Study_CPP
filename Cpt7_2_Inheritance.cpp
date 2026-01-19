5d940bf6d9e69dd2993f52b060593c158eb7fa03
peter6070 <peter6070@naver.com>
2026-01-19 19:32:44 +0900

    [Cpt 7-2] Start Understanding of Inheritance
    챕터 7-2 상속의 이해 시작

---
+++ Cpt7_2_Inheritance.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Person {
+	int age;
+	char name[50];
+public:
+	Person(int myage, const char* myname) : age(myage) {
+		strcpy_s(name, 50, myname);
+	}
+	void WharYourName() const {
+		cout << "My name is " << name << endl;
+	}
+	void HowOldAreYou() const {
+		cout << "I'm " << age << " years old" << endl;
+	}
+};
+
+class UnivStudent : public Person {	//Person 클래스의 상속
+	char major[50];
+public:
+	UnivStudent(const char* myname, int myage, const char* mymajor) 
+		: Person(myage, myname) {
+		strcpy_s(major, 50, mymajor);
+	}
+	void WhoAreYou() const {
+		WharYourName(); //상속 함수
+		HowOldAreYou(); //상속 함수
+		cout << "My major is " << major << endl << endl;
+	}
+};
+
+
+int main(void) {
+	UnivStudent ustd1("Lee", 22, "Computer eng.");
+	ustd1.WhoAreYou();
+	
+	UnivStudent ustd2("Kim", 21, "Electronic eng.");
+	ustd2.WhoAreYou();
+
+
+	return 0;
+}
