3e40f7c5cda8103524003639399bc261ab46e6c4
peter6070 <peter6070@naver.com>
2026-01-27 23:37:11 +0900

    [Cpt 7-4] WIP HAS-A
    챕터 7-4 HAS-A 관계 진행중

--- Cpt7_4_condition_for_Inheritance.cpp
+++ Cpt7_4_condition_for_Inheritance.cpp
@@ -2,71 +2,101 @@
 #include <cstring>
 using namespace std;
 
-//컴퓨터
-class Computer {
+////컴퓨터
+//class Computer {
+//private:
+//	char owner[50];
+//public:
+//	Computer(const char* name) {
+//		strcpy_s(owner, 50, name);
+//	}
+//	void Calculate() {
+//		cout << "Calculate the request";
+//	}
+//};
+//
+////노트북(컴퓨터를 상속함 -> LabtopComp은 Computer이다)
+//class LabtopComp : public Computer {
+//private:
+//	int Battery;
+//public:
+//	LabtopComp(const char* name, int initChag)
+//		: Computer(name), Battery(initChag) {
+//	}
+//	void Charging() { Battery += 5;  }
+//	void UseBattery() { Battery -= 1; }
+//	void MovingCal() {
+//		if (GetBatteryInfo() < 1) {
+//			cout << "Need Charging" << endl;
+//			return;
+//		}
+//		Calculate();
+//		UseBattery();
+//		cout << " as you move" << endl;
+//	}
+//	int GetBatteryInfo() { return Battery; }
+//};
+//
+////태블릿(노트북을 상속함 -> TabletLabtop은 LabtopComp이다 -> TabletLabtop은 Computer이다)
+//class TabletLabtop : public LabtopComp {
+//private:
+//	char regstPenModel[50];
+//public:
+//	TabletLabtop(const char* name, int initChag, const char* pen) 
+//	: LabtopComp(name, initChag)
+//	{
+//		strcpy_s(regstPenModel, 50, pen);
+//	}
+//	void Write(const char* penInfo) {
+//		if (GetBatteryInfo() < 1) {
+//			cout << "Need Charging" << endl;
+//			return;
+//		}
+//		if (strcmp(regstPenModel, penInfo) != 0) {
+//			cout << "Not Registrated pen";
+//			return;
+//		}
+//		cout << "Process Writing contents" << endl;
+//		UseBattery();
+//	}
+//};
+//----------------------------------------------
+
+class Gun {
 private:
-	char owner[50];
+	int bullet;
 public:
-	Computer(const char* name) {
-		strcpy_s(owner, 50, name);
-	}
-	void Calculate() {
-		cout << "Calculate the request";
+	Gun(int bnum) : bullet(bnum) {}
+	void Shot() {
+		cout << "Bang!" << endl;
+		bullet--;
 	}
 };
 
-//노트북(컴퓨터를 상속함 -> LabtopComp은 Computer이다)
-class LabtopComp : public Computer {
+class Police : public Gun {
 private:
-	int Battery;
+	int handcuffs;
 public:
-	LabtopComp(const char* name, int initChag)
-		: Computer(name), Battery(initChag) {
-	}
-	void Charging() { Battery += 5;  }
-	void UseBattery() { Battery -= 1; }
-	void MovingCal() {
-		if (GetBatteryInfo() < 1) {
-			cout << "Need Charging" << endl;
-			return;
-		}
-		Calculate();
-		UseBattery();
-		cout << " as you move" << endl;
-	}
-	int GetBatteryInfo() { return Battery; }
-};
-
-//태블릿(노트북을 상속함 -> TabletLabtop은 LabtopComp이다 -> TabletLabtop은 Computer이다)
-class TabletLabtop : public LabtopComp {
-private:
-	char regstPenModel[50];
-public:
-	TabletLabtop(const char* name, int initChag, const char* pen) 
-	: LabtopComp(name, initChag)
-	{
-		strcpy_s(regstPenModel, 50, pen);
-	}
-	void Write(const char* penInfo) {
-		if (GetBatteryInfo() < 1) {
-			cout << "Need Charging" << endl;
-			return;
-		}
-		if (strcmp(regstPenModel, penInfo) != 0) {
-			cout << "Not Registrated pen";
-			return;
-		}
-		cout << "Process Writing contents" << endl;
-		UseBattery();
+	Police(int bnum, int bcuff) : Gun(bnum), handcuffs(bcuff) {}
+	void PutHandcuff() {
+		cout << "SNAP!" << endl;
+		handcuffs--;
 	}
 };
 
 int main(void) {
-
-	LabtopComp lc("Lee", 5);
+	//IS-A
+	/*LabtopComp lc("Lee", 5);
 	TabletLabtop tl("Jung", 5, "ISE-241-242");
 	lc.MovingCal();
-	tl.Write("ISE-241-242");
+	tl.Write("ISE-241-242");*/
+
+	//----------------------------------------------
+	//HAS-A
+	Police pman(5, 3); //총알 5, 수갑 3
+	pman.Shot();
+	pman.PutHandcuff();
+
 
 	return 0;
 }
