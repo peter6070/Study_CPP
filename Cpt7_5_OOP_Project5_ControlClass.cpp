f4a22262a8f389aae3d432399f1286a37b242f69
peter6070 <peter6070@naver.com>
2026-01-30 00:01:15 +0900

    [Cpt 7-5] improve OOP Project5
    챕터 7-5 OOP 프로젝트5 교재 속 내용으로 개선(함수 정의를 클래스 밖에 한 후 함수들을 클래스의 소속으로 지정)

--- Cpt7_5_OOP_Project5_ControlClass.cpp
+++ Cpt7_5_OOP_Project5_ControlClass.cpp
@@ -14,168 +14,167 @@ class Account {
 	char* cusName;
 	int balance;
 public:
-	Account(int myId, char* nameStr, int myBalance)
-		:id(myId), balance(myBalance)
-	{
-		int len = strlen(nameStr) + 1;
-		cusName = new char[len];
-		strcpy_s(cusName, len, nameStr);
-	}
-	//복사 생성자
-	//동적 할당을 하는 클래스(char*)는 무조건 복사 생성자를 정의해서 깊은 복사를 해야 함(보험으로 작성한 것, 이 코드 예시에서는 작동 X)
-	Account(const Account& copy)
-		: id(copy.id), balance(copy.balance)
-	{
-		int len = strlen(copy.cusName) + 1;
-		cusName = new char[len];
-		strcpy_s(cusName, len, copy.cusName);
-	}
+	Account(int myId, char* nameStr, int myBalance);
+	Account(const Account& copy);
 
-	int GetID() const {
-		return id;
-	}
-	char* GetName() const {
-		return cusName;
-	}
-	int GetBalance() const {
-		return balance;
-	}
-	void DepositBalance(int changeAmount) { //void 형에는 const가 의미없음(const: 리턴값이 상수이다)
-		balance += changeAmount;
-	}
-	int WithdrawalBalance(int changeAmount) { //잔액 수정해야하므로 const 제거
-		if (balance < changeAmount)
-			return 0;
-		balance -= changeAmount;
-
-		return balance; //리턴 빼먹은거 추가
-	}
-	~Account() {
-		delete[] cusName;
-	}
-	void ShowAccInfo() const {
-		cout << "\nAccount ID: " << id << endl;
-		cout << "Name: " << cusName << endl;
-		cout << "Balance: " << balance << endl << endl;
-	}
+	int GetAccID() const;
+	void Deposit(int money);
+	int Withdraw(int money);
+	void ShowAccInfo() const;
+	~Account();
 };
+
+Account::Account(int myId, char* nameStr, int myBalance)
+	:id(myId), balance(myBalance)
+{
+	int len = strlen(nameStr) + 1;
+	cusName = new char[len];
+	strcpy_s(cusName, len, nameStr);
+}
+//복사 생성자
+//동적 할당을 하는 클래스(char*)는 무조건 복사 생성자를 정의해서 깊은 복사를 해야 함(보험으로 작성한 것, 이 코드 예시에서는 작동 X)
+Account::Account(const Account& copy)
+	: id(copy.id), balance(copy.balance)
+{
+	int len = strlen(copy.cusName) + 1;
+	cusName = new char[len];
+	strcpy_s(cusName, len, copy.cusName);
+}
+
+int Account::GetAccID() const {
+	return id;
+}
+//char* GetName() const {
+//	return cusName;
+//}
+//int GetBalance() const {
+//	return balance;
+//}
+void Account::Deposit(int money) { //void 형에는 const가 의미없음(const: 리턴값이 상수이다)
+	balance += money;
+}
+int Account::Withdraw(int money) { //잔액 수정해야하므로 const 제거
+	if (balance < money)
+		return 0;
+	balance -= money;
+
+	return balance; //리턴 빼먹은거 추가
+}
+void Account::ShowAccInfo() const {
+	cout << "\nAccount ID: " << id << endl;
+	cout << "Name: " << cusName << endl;
+	cout << "Balance: " << balance << endl << endl;
+}
+Account::~Account() {
+	delete[] cusName;
+}
+
 
 
 //컨트롤 클래스
 class AccountHandler {
+private:
+	int accNum = 0; //저장된 고객수
+	Account* accArr[MAX_CLIENT]; //계좌정보 클래스 배열
 public:
-	//저장된 고객수
-	int accNum = 0;
+	AccountHandler();
+	void PrintMenu() const;
+	void MakeAccount();
+	void Deposit();
+	void Withdraw();
+	void ShowAllAccInfo() const;
+	~AccountHandler();
 
-	//계좌정보 클래스 배열
-	Account* accArr[MAX_CLIENT];
-public:
-	////함수 선언
-	//void PrintMenu();
-	//void PrintMenu();
-	//void MakeAccount();
-	//void Deposit();
-	//void Withdraw();
-	//void ShowAllAccInfo();
-	//함수 정의
+};
+//함수 정의
 
 //메뉴 출력
-	void PrintMenu() {
-		cout << "----Menu----" << endl;
-		cout << "1. Account opening" << endl;
-		cout << "2. Deposit" << endl;
-		cout << "3. WithDrawal" << endl;
-		cout << "4. Print all accout Info" << endl;
-		cout << "5. Exit program" << endl;
-		cout << "Select Number: ";
+void AccountHandler::PrintMenu() const {
+	cout << "----Menu----" << endl;
+	cout << "1. Account opening" << endl;
+	cout << "2. Deposit" << endl;
+	cout << "3. WithDrawal" << endl;
+	cout << "4. Print all accout Info" << endl;
+	cout << "5. Exit program" << endl;
+	cout << "Select Number: ";
+}
+
+//계좌 개설
+void AccountHandler::MakeAccount() {
+	int id;
+	char name[100];
+	int balance;
+	cout << "[Account opening]" << endl;
+	cout << "Account ID: ";
+	cin >> id;
+	cout << "Name: ";
+	cin >> name;
+	cout << "Deposit amount: ";
+	cin >> balance;
+	accArr[accNum++] = new Account(id, name, balance);
+}
+
+//입금
+void AccountHandler::Deposit() {
+	int searchID = 0; //입금 계좌 ID
+	int changeAmount = 0; //추가 입금액
+
+	cout << "[Deposit]" << endl;
+	cout << "Account ID: ";
+	cin >> searchID;
+	for (int i = 0; i < accNum; i++) { //id 찾기
+		if (searchID == accArr[i]->GetAccID()) {
+			cout << "Deposit amount: ";
+			cin >> changeAmount;
+			accArr[i]->Deposit(changeAmount);
+			cout << "Deposit Complete" << endl;
+			return;
+		}
 	}
+	cout << "ID Not Found" << endl;
+}
 
-	//계좌 개설
-	void MakeAccount() {
-		int id;
-		char name[100];
-		int balance;
-		cout << "[Account opening]" << endl;
-		cout << "Account ID: ";
-		cin >> id;
-		cout << "Name: ";
-		cin >> name;
-		cout << "Deposit amount: ";
-		cin >> balance;
-		accArr[accNum++] = new Account(id, name, balance);
-		/*accNum++;*/
-	}
+//출금
+void AccountHandler:: Withdraw() {
+	int searchID = 0; //입금 계좌 ID
+	int changeAmount = 0; //추가 입금액
 
-	//입금
-	void Deposit() {
-		int searchID = 0; //입금 계좌 ID
-		int changeAmount = 0; //추가 입금액
-		//bool isIDFound = false; //ID 검색 확인 여부
-
-		cout << "[Deposit]" << endl;
-		cout << "Account ID: ";
-		cin >> searchID;
-		for (int i = 0; i < accNum; i++) { //id 찾기
-			if (searchID == accArr[i]->GetID()) {
-				//isIDFound = true;
-				cout << "Deposit amount: ";
-				cin >> changeAmount;
-				accArr[i]->DepositBalance(changeAmount);
-				cout << "Deposit Complete" << endl;
+	cout << "[WithDrawal]" << endl;
+	cout << "Account ID: ";
+	cin >> searchID;
+	for (int i = 0; i < accNum; i++) { //id 찾기
+		if (searchID == accArr[i]->GetAccID()) {
+			cout << "Withdrawal amount: ";
+			cin >> changeAmount;
+			if (accArr[i]->Withdraw(changeAmount) == 0) {
+				cout << "Balance shortage" << endl;
 				return;
 			}
-		}
-		//if (!isIDFound)
-		cout << "ID Not Found" << endl;
-	}
-
-	//출금
-	void Withdraw() {
-		int searchID = 0; //입금 계좌 ID
-		int changeAmount = 0; //추가 입금액
-		//bool isIDFound = false; //ID 검색 확인 여부
-
-		cout << "[WithDrawal]" << endl;
-		cout << "Account ID: ";
-		cin >> searchID;
-		for (int i = 0; i < accNum; i++) { //id 찾기
-			if (searchID == accArr[i]->GetID()) {
-				//isIDFound = true;
-				cout << "Withdrawal amount: ";
-				cin >> changeAmount;
-				if (accArr[i]->WithdrawalBalance(changeAmount) == 0) {
-					cout << "Balance shortage" << endl;
-					return;
-				}
-				cout << "Withdrawal Complete" << endl;
-				return;
-			}
-		}
-		//if (!isIDFound)
-		cout << "ID Not Found" << endl;
-	}
-
-	////모든 고객 계좌 정보 출력
-	void ShowAllAccInfo() {
-		for (int i = 0; i < accNum; i++) {
-			/*cout << "\nAccount ID: " << accArr[i]->GetID() << endl;
-			cout << "Name: " << accArr[i]->GetName() << endl;
-			cout << "Balance: " << accArr[i]->GetBalance() << endl << endl;*/
-			accArr[i]->ShowAccInfo();
+			cout << "Withdrawal Complete" << endl;
+			return;
 		}
 	}
-};
+	cout << "ID Not Found" << endl;
+}
 
+AccountHandler::AccountHandler() : accNum(0){}
 
+//모든 고객 계좌 정보 출력
+void AccountHandler::ShowAllAccInfo() const {
+	for (int i = 0; i < accNum; i++) {
+		accArr[i]->ShowAccInfo();
+		cout << endl;
+	}
+}
 
-
-
+AccountHandler::~AccountHandler() {
+	for (int i = 0; i < accNum; i++)
+		delete accArr[i];
+}
 
 
 int main(void) {
-
 	AccountHandler accHan;
-
 	int selectNum = 0; //번호 선택
 
 	while (true) {
@@ -197,9 +196,6 @@ int main(void) {
 			break;
 		case 5:
 			cout << "Pragram Exit" << endl;
-			for (int i = 0; i < accHan.accNum; i++) {
-				delete accHan.accArr[i];
-			}
 			return 0;
 		default:
 			cout << "Illegal selection.." << endl;
