53a2b00f0cfb2f7bd340d8c77aeedf687ee06195
peter6070 <peter6070@naver.com>
2026-02-02 23:52:40 +0900

    [Cpt 8-3] Complete Reference Possibility
    챕터 8-3 참조자의 참조가능성 완료

--- Cpt8_3_VirtualDestructor_ReferencePossibility.cpp
+++ Cpt8_3_VirtualDestructor_ReferencePossibility.cpp
@@ -1,35 +1,82 @@
 #include <iostream>
 using namespace std;
 
+//class First {
+//private:
+//	char* strOne;
+//public:
+//	First(const char* str) {
+//		strOne = new char[strlen(str) + 1];
+//	}
+//	virtual ~First() {
+//		cout << "~First()" << endl;
+//		delete[] strOne;
+//	}
+//};
+//
+//class Second : public First {
+//private:
+//	char* strTwo;
+//public:
+//	Second(const char* str1, const char* str2) :First(str1) {
+//		strTwo = new char[strlen(str2) + 1];
+//	}
+//	virtual ~Second() {
+//		cout << "~Second()"<<endl;
+//		delete[] strTwo;
+//	}
+//};
+//---------------------------------------------------
+
 class First {
-private:
-	char* strOne;
 public:
-	First(const char* str) {
-		strOne = new char[strlen(str) + 1];
+	void FirstFunc() {
+		cout << "FirstFunc()" << endl;
 	}
-	virtual ~First() {
-		cout << "~First()" << endl;
-		delete[] strOne;
+	virtual void SimpleFunc() {
+		cout << "First's SimpleFunc()" << endl;
 	}
 };
 
-class Second : public First {
-private:
-	char* strTwo;
+class Second :public First {
 public:
-	Second(const char* str1, const char* str2) :First(str1) {
-		strTwo = new char[strlen(str2) + 1];
+	void SecondFunc() {
+		cout << "SecondFunc()" << endl;
 	}
-	virtual ~Second() {
-		cout << "~Second()"<<endl;
-		delete[] strTwo;
+	virtual void SimpleFunc() {
+		cout << "Second's SimpleFunc()" << endl;
+	}
+};
+
+class Third :public Second {
+public:
+	void ThirdFunc() {
+		cout << "ThirdFunc()" << endl;
+	}
+	virtual void SimpleFunc() {
+		cout << "Third's SimpleFunc()" << endl;
 	}
 };
 
 int main(void) {
-	First* ptr = new Second("simple", "complex");
-	delete ptr;
+	//First* ptr = new Second("simple", "complex");
+	//delete ptr;
+	//---------------------------------------------------
+
+	Third obj;
+	obj.FirstFunc();
+	obj.SecondFunc();
+	obj.ThirdFunc();
+	obj.SimpleFunc();
+
+	Second& sref = obj; 
+	sref.FirstFunc();
+	sref.SecondFunc();
+	sref.SimpleFunc(); //sref가 참조하는 객체가 Third이기 때문에 가상함수로 선언된 SimpleFunc는 실제 객체에서 함수 호출
+
+	First& fref = obj;
+	fref.FirstFunc();
+	fref.SimpleFunc();
 
 	return 0;
 }
