8f6581ee15a6c471453c275e38b82396d7d12134
peter6070 <peter6070@naver.com>
2026-02-04 21:49:42 +0900

    [Cpt 9-1] Complete Virtual Function principle
    챕터 9-1 가상함수 원리 완료

--- Cpt9_1_MemberFunc_VirtualFunc.cpp
+++ Cpt9_1_MemberFunc_VirtualFunc.cpp
@@ -9,33 +9,55 @@ using namespace std;
 //	void ShowData() { cout << "Data: " << data << endl; }
 //	void Add(int num) { data += num; }
 //};
+//-----------------------------------------------
+////Data 클래스를 흉내 낸 영역(멤버변수 작동원리 이해)
+//typedef struct Data {
+//	int data;
+//	//함수 포인터 변수가 구조체 멤버로 등장(함수 정의)
+//	void (*ShowData)(Data*);
+//	void (*Add)(Data*, int);
+//
+//} Data;
+////함수 선언
+//void ShowData(Data* THIS) { cout << "Data: " << THIS->data << endl; }
+//void Add(Data* THIS, int num) { THIS->data += num; }
+//-----------------------------------------------
 
-//Data 클래스를 흉내 낸 영역
-typedef struct Data {
-	int data;
-	//함수 포인터 변수가 구조체 멤버로 등장(함수 정의)
-	void (*ShowData)(Data*);
-	void (*Add)(Data*, int);
+class AAA {
+private:
+	int num1;
+public:
+	virtual void Func1() { cout << "Func1" << endl; }
+	virtual void Func2() { cout << "Func2" << endl; }
+};
 
-} Data;
-
-//함수 선언
-void ShowData(Data* THIS) { cout << "Data: " << THIS->data << endl; }
-void Add(Data* THIS, int num) { THIS->data += num; }
+class BBB : public AAA {
+private:
+	int num2;
+public:
+	virtual void Func1() { cout << "BBB::Func1" << endl; }
+	void Func3() { cout << "Func3" << endl; }
+};
 
 int main(void) {
 	/*Data obj(15);
 	obj.Add(17);
 	obj.ShowData();*/
+	//-----------------------------------------------
+	////구조체(객체) 생성
+	//Data obj1 = { 15,ShowData,Add };
+	//Data obj2 = { 7,ShowData,Add };
 
-	//구조체(객체) 생성
-	Data obj1 = { 15,ShowData,Add };
-	Data obj2 = { 7,ShowData,Add };
+	//obj1.Add(&obj1, 17); //obj1 주소에서 Add 함수를 실행
+	//obj2.Add(&obj2, 9);
+	//obj1.ShowData(&obj1); //obj1 주소에서 ShowData 함수를 실행
+	//obj2.ShowData(&obj2);
+	//-----------------------------------------------
+	AAA* aptr = new AAA();
+	aptr->Func1();
 
-	obj1.Add(&obj1, 17);
-	obj2.Add(&obj2, 9);
-	obj1.ShowData(&obj1);
-	obj2.ShowData(&obj2);
+	BBB* bptr = new BBB();
+	bptr -> Func1();
 
 	return 0;
 }
