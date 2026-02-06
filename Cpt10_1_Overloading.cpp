f64d9b70fb02d5cffa408c14db8d1fdcdab030df
peter6070 <peter6070@naver.com>
2026-02-06 23:35:38 +0900

    [Cpt 10-1] Complete Q1
    챕터 10-1 문제 1 완료( - 연산자 오버로딩)

--- Cpt10_1_Overloading.cpp
+++ Cpt10_1_Overloading.cpp
@@ -9,11 +9,25 @@ public:
 	void ShowPosition() const {
 		cout << '[' << xpos << ", " << ypos << ']' << endl;
 	}
-	Point operator+(const Point& ref) {
-		Point pos(xpos + ref.xpos, ypos + ref.ypos);
-		return pos;
-	}
+	friend Point operator+(const Point& pos1, const Point& pos2);
+	//private 영역 접근 허용을 위해 friend 선언
+	//friend 선언을 통해 전역함수인 operator+ 가 Point의 private 영역에 접근 가능 
+	// 또한 Point 클래스가 + 연산에 대해 연산자 오버로딩이 되어있음을 알 수 있음
+	// 
+	//문제2. - 연산자 오버로딩
+	friend Point operator-(const Point& pos1, const Point& pos2);
 };
+
+//전역함수 기반의 오버로딩 함수
+Point operator+(const Point &pos1, const Point &pos2) {
+	Point pos(pos1.xpos + pos2.xpos, pos1.ypos + pos2.ypos);
+	return pos;
+}
+//문제2. - 연산자 오버로딩
+Point operator-(const Point& pos1, const Point& pos2) {
+	Point pos(pos1.xpos - pos2.xpos, pos1.ypos - pos2.ypos);
+	return pos;
+}
 
 int main(void) {
 
@@ -21,10 +35,12 @@ int main(void) {
 	Point pos2(10, 20);
 	//Point pos3 = pos1.operator+(pos2);
 	Point pos3 = pos1+pos2;
+	Point pos4 = pos2 - pos1;
 
 	pos1.ShowPosition();
 	pos2.ShowPosition();
 	pos3.ShowPosition();
+	pos4.ShowPosition();
 
 	return 0;
 }
