b0916678c90b0ab20fdf7e3cb776c3d289b15eb5
peter6070 <peter6070@naver.com>
2026-02-06 23:58:10 +0900

    [Cpt 10-1] Complete Q2, 3
    챕터 10-1 문제 2,3(-,+=,-=,==,!= 연산자 오버로딩) 완료

--- Cpt10_1_Overloading.cpp
+++ Cpt10_1_Overloading.cpp
@@ -13,9 +13,24 @@ public:
 	//private 영역 접근 허용을 위해 friend 선언
 	//friend 선언을 통해 전역함수인 operator+ 가 Point의 private 영역에 접근 가능 
 	// 또한 Point 클래스가 + 연산에 대해 연산자 오버로딩이 되어있음을 알 수 있음
-	// 
-	//문제2. - 연산자 오버로딩
+	
+	//문제1. - 연산자 오버로딩
 	friend Point operator-(const Point& pos1, const Point& pos2);
+	//문제3. ==,!= 연산자 오버로딩
+	friend bool operator==(const Point& pos1, const Point& pos2);
+	friend bool operator!=(const Point& pos1, const Point& pos2);
+
+	//문제2.+=, -= 연산자 오버로딩
+	Point operator+=(const Point& ref) {
+		xpos += ref.xpos;
+		ypos += ref.ypos;
+		return *this;
+	}
+	Point operator-=(const Point& ref) {
+		xpos -= ref.xpos;
+		ypos -= ref.ypos;
+		return *this;
+	}
 };
 
 //전역함수 기반의 오버로딩 함수
@@ -23,24 +38,48 @@ Point operator+(const Point &pos1, const Point &pos2) {
 	Point pos(pos1.xpos + pos2.xpos, pos1.ypos + pos2.ypos);
 	return pos;
 }
-//문제2. - 연산자 오버로딩
+//문제1. - 연산자 오버로딩
 Point operator-(const Point& pos1, const Point& pos2) {
 	Point pos(pos1.xpos - pos2.xpos, pos1.ypos - pos2.ypos);
 	return pos;
 }
 
+//문제3. ==,!= 연산자 오버로딩
+bool operator==(const Point& pos1, const Point& pos2) {
+	if (pos1.xpos == pos2.xpos && pos1.ypos == pos2.ypos)
+		return true;
+	else
+		return false;
+}
+bool operator!=(const Point& pos1, const Point& pos2) {
+	return !(pos1==pos2); //오버로딩된 == 연산자 호출
+}
+
 int main(void) {
 
-	Point pos1(3, 4);
-	Point pos2(10, 20);
-	//Point pos3 = pos1.operator+(pos2);
-	Point pos3 = pos1+pos2;
-	Point pos4 = pos2 - pos1;
+	//Point pos1(3, 4);
+	//Point pos2(10, 20);
+	////Point pos3 = pos1.operator+(pos2);
+	//Point pos3 = pos1+pos2;
 
-	pos1.ShowPosition();
-	pos2.ShowPosition();
-	pos3.ShowPosition();
-	pos4.ShowPosition();
+	Point pos1(20, 30);
+	Point pos2(5, 7);
+	Point pos3(5, 7);
+
+	(pos1 - pos2).ShowPosition();
+	(pos2 += pos3).ShowPosition();
+
+	if (pos2 == pos3)
+		cout << "equal" << endl;
+	else
+		cout << "not equal" << endl;
+
+	(pos2 -= pos3).ShowPosition();
+
+	if(pos2!=pos3)
+		cout << "not equal" << endl;
+	else
+		cout << "equal" << endl;
 
 	return 0;
 }
