3f6e13c6f268b61b3f40f57352dabf18d06539da
peter6070 <peter6070@naver.com>
2026-02-10 23:17:08 +0900

    [Cpt 10-4] WIP << Operator
    챕터 10-4 <<연산자 진행중

--- Cpt10_4_Cout_Cin_Endl.cpp
+++ Cpt10_4_Cout_Cin_Endl.cpp
@@ -2,9 +2,10 @@
 namespace mystd {
 	using namespace std;
 
+	//출력 클래스
 	class ostream {
 	public:
-		void operator<< (const char* str) {
+		/*void operator<< (const char* str) {
 			printf("%s", str);
 		}
 		void operator<<(char str) {
@@ -15,19 +16,40 @@ namespace mystd {
 		}
 		void operator<< (double e) {
 			printf("%g", e);
+		}*/
+		//-----------------------------------------------
+		// << 연산자 연속해서 사용시
+		ostream& operator<< (const char* str) {
+			printf("%s", str);
+			return *this;
 		}
-		void operator<<(ostream& (*fp)(ostream& ostm)) { //함수 포인터
-			fp(*this);
+		ostream& operator<<(char str) {
+			printf("%c", str);
+			return *this;
+		}
+		ostream& operator<<(int num) {
+			printf("%d", num);
+			return *this;
+		}
+		ostream& operator<< (double e) {
+			printf("%g", e);
+			return *this;
+		}
+		//함수 그 자체를 전달받아서 실행해주는 연산자
+		ostream& operator<<(ostream& (*fp)(ostream& ostm)) { //함수 포인터(*fp)
+					  //반환형  포인터 이름  매개변수
+			return fp(*this);
 		}
 	};
 
+	//endl 함수
 	ostream& endl(ostream& ostm) {
 		ostm << '\n';
 		fflush(stdout);
 		return ostm;
 	}
 
-	ostream cout;
+	ostream cout; //변수명(ostream 클래스 기능을 가진 객체를 만들고 이름을 cout이라고 짓겠다
 }
 
 
@@ -36,12 +58,16 @@ int main(void) {
 	using mystd::cout;
 	using mystd::endl;
 
-	cout << "Simple String";
-	cout << endl;
-	cout << 3.14;
-	cout << endl;
-	cout << 123;
-	endl(cout);
+	//cout << "Simple String"; //-> cout.operator<<("Simple String");
+	//cout << endl; //-> cout.operator<<(endl);
+	//cout << 3.14; //-> cout.operator<<(3.14);
+	//cout << endl; //-> cout.operator<<(endl);
+	//cout << 123; //-> cout.operator<<(123);
+	//endl(cout);
+
+	//한줄로 작성
+	cout << 3.14 << endl << 123 << endl << 'a' << endl;
+	//-> (((((cout << 3.14) << endl) << 123) << endl) << 'a') << endl;
 
 	return 0;
 }
