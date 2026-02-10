f9141f5c5919e773ef0e7e9d3e89d2de12139dab
peter6070 <peter6070@naver.com>
2026-02-10 22:25:25 +0900

    [Cpt 10-4] WIP endl
    챕터 10-4 endl 진행중

--- Cpt10_4_Cout_Cin_Endl.cpp
+++ Cpt10_4_Cout_Cin_Endl.cpp
@@ -1,11 +1,47 @@
 #include <iostream>
-using namespace std;
+namespace mystd {
+	using namespace std;
+
+	class ostream {
+	public:
+		void operator<< (const char* str) {
+			printf("%s", str);
+		}
+		void operator<<(char str) {
+			printf("%c", str);
+		}
+		void operator<<(int num) {
+			printf("%d", num);
+		}
+		void operator<< (double e) {
+			printf("%g", e);
+		}
+		void operator<<(ostream& (*fp)(ostream& ostm)) { //함수 포인터
+			fp(*this);
+		}
+	};
+
+	ostream& endl(ostream& ostm) {
+		ostm << '\n';
+		fflush(stdout);
+		return ostm;
+	}
+
+	ostream cout;
+}
 
 
 
 int main(void) {
+	using mystd::cout;
+	using mystd::endl;
 
-
+	cout << "Simple String";
+	cout << endl;
+	cout << 3.14;
+	cout << endl;
+	cout << 123;
+	endl(cout);
 
 	return 0;
 }
