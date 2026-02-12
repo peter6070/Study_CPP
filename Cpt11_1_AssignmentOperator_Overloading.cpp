7aee06dbbf0a69a00629ecb67e619b9759c16072
peter6070 <peter6070@naver.com>
2026-02-12 22:50:12 +0900

    [Cpt 11-1] Complete Q1
    챕터 11-1 문제1(깊은 복사 대입연산자, 복사생성자) 완료

--- Cpt11_1_AssignmentOperator_Overloading.cpp
+++ Cpt11_1_AssignmentOperator_Overloading.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
 
-class First {
-private:
-	int num1, num2;
-public:
-	First(int n1=0, int n2=0):num1(n1), num2(n2){}
-	void ShowData() { cout << num1 << ", " << num2 << endl; }
-	//자동으로 삽입되어 작성 필요X
-	First& operator=(const First& ref) {
-		cout << "First& operator=()" << endl;
-		num1 = ref.num1;
-		num2 = ref.num2;
-		return *this;
-	}
-};
+//class First {
+//private:
+//	int num1, num2;
+//public:
+//	First(int n1=0, int n2=0):num1(n1), num2(n2){}
+//	void ShowData() { cout << num1 << ", " << num2 << endl; }
+//	//자동으로 삽입되어 작성 필요X
+//	First& operator=(const First& ref) {
+//		cout << "First& operator=()" << endl;
+//		num1 = ref.num1;
+//		num2 = ref.num2;
+//		return *this;
+//	}
+//};
 
 //class Second {
 //private:
@@ -32,25 +32,25 @@ public:
 //	}
 //};
 
-class Second :public First {
-private:
-	int num3, num4;
-public:
-	Second(int n1, int n2, int n3, int n4) 
-		: First(n1, n2), num3(n3), num4(n4) {}
-	void ShowData() { 
-		First::ShowData();
-		cout << num3 << ", " << num4 << endl; 
-	}
-
-	Second& operator=(const Second& ref) {
-		cout << "Second& operator=()" << endl;
-		First::operator=(ref); //기초 클래스 대입 연산자 호출
-		num3 = ref.num3;
-		num4 = ref.num4;
-		return *this;
-	}
-};
+//class Second :public First {
+//private:
+//	int num3, num4;
+//public:
+//	Second(int n1, int n2, int n3, int n4) 
+//		: First(n1, n2), num3(n3), num4(n4) {}
+//	void ShowData() { 
+//		First::ShowData();
+//		cout << num3 << ", " << num4 << endl; 
+//	}
+//
+//	Second& operator=(const Second& ref) {
+//		cout << "Second& operator=()" << endl;
+//		First::operator=(ref); //기초 클래스 대입 연산자 호출
+//		num3 = ref.num3;
+//		num4 = ref.num4;
+//		return *this;
+//	}
+//};
 //---------------------------------------------------------
 
 //class Person {
@@ -82,6 +82,67 @@ public:
 //	}
 //};
 
+//문제1. 클래스 간 깊은 복사(대입 연산자, 복사 생성자 정의)
+class Gun {
+private:
+	int bullet;
+public:
+	Gun(int bnum) : bullet(bnum) {}
+	void Shot() {
+		cout << "Bang!" << endl;
+		bullet--;
+	}
+};
+
+class Police {
+private:
+	int handcuffs;
+	Gun* pistol;
+public:
+	Police(int bnum, int bcuff) : handcuffs(bnum) {
+		if (bnum > 0)
+			pistol = new Gun(bnum);
+		else
+			pistol = NULL;
+	}
+	void PutHandcuff() {
+		cout << "SNAP!" << endl;
+		handcuffs--;
+	}
+	void Shot() {
+		if (pistol == NULL)
+			cout << "No Armo!" << endl;
+		else
+			pistol->Shot();
+	}
+	~Police() {
+		if (pistol != NULL)
+			delete pistol;
+	}
+	//대입 연산자
+	Police& operator=(const Police& ref) {
+		cout << "Police& operator=(const Police& ref)" << endl;
+		if(pistol!=NULL)
+			delete[] pistol;
+
+		if (ref.pistol != NULL)
+			pistol = new Gun(*(ref.pistol));
+		else
+			pistol = NULL;
+
+		handcuffs = ref.handcuffs;
+		return *this;
+	}
+	//복사 생성자
+	Police(const Police& copy) : handcuffs(copy.handcuffs) {
+		cout << "Police(const Police& copy)" << endl;
+		if (copy.pistol != NULL)
+			pistol = new Gun(*(copy.pistol));
+		else
+			pistol = NULL;
+	}
+};
+
 int main(void) {
 	//First fsrc(111, 222);
 	//First fcpy;
@@ -109,11 +170,22 @@ int main(void) {
 	//man1.ShowPersonInfo();
 	//man2.ShowPersonInfo();
 	//---------------------------------------------------------
-	Second ssrc(111, 222, 333, 444);
+	/*Second ssrc(111, 222, 333, 444);
 	Second scpy(0, 0, 0, 0);
 	scpy = ssrc;
-	scpy.ShowData();
+	scpy.ShowData();*/
+	//---------------------------------------------------------
+	// 
+	//문제1. 클래스 간 깊은 복사(대입 연산자, 복사 생성자 정의)
+	Police pol1(5, 2);
+	Police pol2 = pol1; //복사 생성자 호출
+	pol2.PutHandcuff();
+	pol2.Shot();
 
+	Police pol3(2, 4);
+	pol3 = pol1; //대입 연산자 호출
+	pol3.PutHandcuff();
+	pol3.Shot();
 
 	return 0;
 }
