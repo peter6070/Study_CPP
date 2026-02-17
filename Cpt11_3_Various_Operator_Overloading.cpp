01624110a3c71e0577b91a74b73f66475438297c
peter6070 <peter6070@naver.com>
2026-02-17 22:24:51 +0900

    [Cpt 11-3] WIP new, delete Operator Overloading
    챕터 11-3 new, delete 연산자 오버로딩 진행중

--- Cpt11_3_Various_Operator_Overloading.cpp
+++ Cpt11_3_Various_Operator_Overloading.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 using namespace std;
 
+class Point {
+private:
+	int xpos, ypos;
+public:
+	Point(int x = 0, int y=0):xpos(x), ypos(y){}
+	friend ostream& operator<<(ostream& os, const Point& pos);
 
+	void* operator new (size_t size) {
+		cout << "oeprator new : " << size << endl;
+		void* adr = new char[size]; //바이트 단위로 필요 메모리 공간 할당
+		//char형을 대상으로 new 연산을 하는 문장은 malloc함수의 호출문으로 대신할 수 있음
+		return adr;
+	}
+	void operator delete(void* adr) {
+		cout << "operator delete () " << endl;
+		delete[]adr;
+		//free함수 호출문으로 대신할 수 있음
+	}
+};
+
+ostream& operator<<(ostream& os, const Point& pos) {
+	os << '[' << pos.xpos << ", " << pos.ypos << ']' << endl;
+	return os;
+}
 
 int main(void) {
-
+	Point* ptr = new Point(3, 4);
+	cout << *ptr;
+	delete ptr;
 
 
 	return 0;
